c65f997bc345bc6655a9c68d981e3ffe5347a6fb
Dylan <leehc12dl@naver.com>
2026-01-22 11:11:31 +0900

    backend

--- backend/src/test/java/noonchissaum/backend/BackendApplicationTests.java
+++ backend/src/test/java/noonchissaum/backend/BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package noonchissaum.igloo;
+package noonchissaum.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IgLooApplicationTests {
+class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
