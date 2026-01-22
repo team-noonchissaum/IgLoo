f3248621da2210fed0b53c6f1bfdf5b99412b583
Dylan <leehc12dl@naver.com>
2026-01-22 11:08:08 +0900

    backend

---
+++ src/main/java/noonchissaum/igloo/IgLooApplication.java
@@ -0,0 +1,13 @@
+package noonchissaum.igloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgLooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgLooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/noonchissaum/igloo/IgLooApplicationTests.java
@@ -0,0 +1,13 @@
+package noonchissaum.igloo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgLooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
