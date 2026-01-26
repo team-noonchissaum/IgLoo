b4c452dd1bbe5d59f5f8a23b9396e865a064dd1b
Dylan <leehc12dl@naver.com>
2026-01-26 17:03:37 +0900

    refactor: 테스트용 수정

--- backend/src/main/java/noonchissaum/backend/domain/auction/repository/BidRepository.java
+++ backend/src/main/java/noonchissaum/backend/domain/auction/repository/BidRepository.java
@@ -1,7 +1,12 @@
 package noonchissaum.backend.domain.auction.repository;
 
+import noonchissaum.backend.domain.auction.entity.Auction;
 import noonchissaum.backend.domain.auction.entity.Bid;
+import noonchissaum.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BidRepository extends JpaRepository<Bid,Long> {
+    Optional<Bid> findByAuctionAndBidder(Auction auction, User user);
 }
