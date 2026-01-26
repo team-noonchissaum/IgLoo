9970244c5853c8b2692e6af887593a46b5a3c857
Dylan <leehc12dl@naver.com>
2026-01-26 21:49:58 +0900

    refactor : bid record부분을 좀 더 섬세하게 수정

--- backend/src/main/java/noonchissaum/backend/domain/auction/scheduler/PendingBidScheduler.java
+++ backend/src/main/java/noonchissaum/backend/domain/auction/scheduler/PendingBidScheduler.java
@@ -2,11 +2,8 @@ package noonchissaum.backend.domain.auction.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import noonchissaum.backend.domain.auction.entity.Auction;
-import noonchissaum.backend.domain.auction.repository.AuctionRepository;
 import noonchissaum.backend.domain.auction.repository.BidRepository;
 import noonchissaum.backend.domain.auction.service.BidRecordService;
-import noonchissaum.backend.domain.user.entity.User;
 import noonchissaum.backend.domain.wallet.entity.Wallet;
 import noonchissaum.backend.domain.wallet.repository.WalletRepository;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -26,8 +23,6 @@ public class PendingBidScheduler {
     private final BidRepository bidRepository;
     private final BidRecordService bidRecordService;
     private final WalletRepository walletRepository;
-    private final AuctionService auctionService;
-    private final UserService userService;
 
     @Scheduled(fixedDelay = 5 * 60 * 1000) // 5분마다
     public void reconcilePendingBids() {
@@ -56,12 +51,9 @@ public class PendingBidScheduler {
                 String rawRefund = (String) info.get("refundAmount");
                 BigDecimal refundAmount = (rawRefund == null || rawRefund.isBlank()) ? BigDecimal.ZERO : new BigDecimal(rawRefund);
 
-                Auction auction = auctionService.getAuction(auctionId);
-                User user = userService.getUser(userId);
-
                 // Bid 저장
                 if (!bidRepository.existsByRequestId(requestId)) {
-                    bidRecordService.saveBidRecord(auction, user, bidAmount, requestId);
+                    bidRecordService.saveBidRecord(auctionId, userId, bidAmount, requestId);
                 }
 
                 // wallet 저장
