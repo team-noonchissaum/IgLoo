afef6d65848a890c758c89ede7d68f45985ccd1b
koochanghyen <kuku0746@naver.com>
2026-01-28 14:59:04 +0900

    에러코드,의존성추가

--- backend/src/main/java/noonchissaum/backend/domain/user/entity/User.java
+++ backend/src/main/java/noonchissaum/backend/domain/user/entity/User.java
@@ -2,6 +2,7 @@ package noonchissaum.backend.domain.user.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import noonchissaum.backend.domain.auth.entity.UserAuth;
