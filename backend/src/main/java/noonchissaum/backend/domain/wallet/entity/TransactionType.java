a592d2b051ecbbcd16d01902208b1464d485976c
yushinKim <dbtls0218@naver.com>
2026-01-30 10:05:51 +0900

    refactor : TransactionType memo , RefType 일부 변경

--- backend/src/main/java/noonchissaum/backend/domain/wallet/entity/TransactionType.java
+++ backend/src/main/java/noonchissaum/backend/domain/wallet/entity/TransactionType.java
@@ -8,8 +8,8 @@ public enum TransactionType {
 
     CHARGE("충전", TransactionRefType.CHARGE),
 
-    BID_HOLD("입찰 홀딩", TransactionRefType.AUCTION),
-    BID_RELEASE("입찰 해제/환급", TransactionRefType.AUCTION),
+    BID_HOLD("입찰 홀딩", TransactionRefType.BID),
+    BID_RELEASE("입찰 해제/환급", TransactionRefType.BID),
 
     DEPOSIT_LOCK("보증금 락", TransactionRefType.AUCTION),
     DEPOSIT_RETURN("보증금 반환", TransactionRefType.AUCTION),
@@ -17,8 +17,8 @@ public enum TransactionType {
 
     WITHDRAW("출금", TransactionRefType.WITHDRAW),
 
-    SETTLEMENT_IN("정산 입금", TransactionRefType.ORDER),
-    SETTLEMENT_OUT("정산 출금", TransactionRefType.ORDER);
+    SETTLEMENT_IN("판매 정산금 지급", TransactionRefType.ORDER),
+    SETTLEMENT_OUT("제품 구매", TransactionRefType.ORDER);
 
     private final String memo;
     private final TransactionRefType defaultRefType;
