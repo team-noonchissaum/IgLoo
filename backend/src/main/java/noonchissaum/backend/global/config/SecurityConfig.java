2d8bcb54a7373fc2a3dd006b720971bdd8fc6500
koochanghyen <kuku0746@naver.com>
2026-02-06 11:12:27 +0900

    fix:UserEntity&UserAuth

--- backend/src/main/java/noonchissaum/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/noonchissaum/backend/global/config/SecurityConfig.java
@@ -135,7 +135,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(java.util.List.of("http://localhost:3000/"));
+        config.setAllowedOrigins(java.util.List.of("https://ig-loo-fe-89f2.vercel.app"));
         config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(java.util.List.of("*"));
         config.setAllowCredentials(true);
